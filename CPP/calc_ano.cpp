35729c369021070d2f1471c8a50133bcf31010bd
jeansilva730 <144482673+jeansilva730@users.noreply.github.com>
2024-03-22 17:09:58 -0300

    Create calc_ano.cpp

---
+++ CPP/calc_ano.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    dia, mes, ano = 0;
+
+    std::cin >> dia >> mes >> ano;
+    
+    std::cout << "Hello World at: " << dia << "/" << mes << "/" << ano;
+    return 0;
+}
